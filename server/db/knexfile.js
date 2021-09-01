3d38744e42e3da45052b08df81ebe2ea1b2d2b13
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 <p.kakanat@gmail.com>
2021-09-01 12:39:01 +1200

    knexJs failed to resolved config file.

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,13 +1,19 @@
 // Update with your config settings.
-const path = require('path')
 
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.join(__dirname, 'dev.sqlite3')
+      filename: './dev.sqlite3'
     },
+    // },
+    // migrations: {
+    //   directory: './migrations'
+    // },
+    // seeds: {
+    //   directory: './seeds'
+    // },
     useNullAsDefault: true
   },
 
