42a950044b63cc2048ee17628e4c2634eb586a94
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 <p.kakanat@gmail.com>
2021-09-07 13:09:19 +1200

    DUMMY GRAPH HAS APPEAR

--- client/components/BarChart.jsx
+++ client/components/BarChart.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef } from 'react'
-import * as d3 from 'd3'
+import React, { useEffect, useRef, useState } from 'react'
+import { drawChart } from './charts/barchart'
 
 function BarChart () {
+  const [data, setData] = useState([])
   const d3chart = useRef()
 
   useEffect(() => {
@@ -9,21 +10,17 @@ function BarChart () {
       .then(response => response.json())
       .then(data => {
         console.log(data)
-
-        const margin = { top: 50, right: 30, bottom: 30, left: 30 }
-        const width = parseInt(d3.select('#barchart').style('width'))
-        const height = parseInt(d3.select('#barchart').style('height'))
-
-        const svg = d3.select(d3chart.current)
-          .attr('width', width)
-          .attr('height', height)
-          .style('background-color', 'red')
-          .append('g')
-          .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+        const count = []
+        data.map(person => {
+          return count.push(person.age)
+        })
+        setData(count)
+        drawChart(400, 600, count)
         return null
       })
       .catch(e => console.log(e.message))
   }, [])
+
   return (
     <div id='barchart'>
       <svg ref={d3chart}></svg>
---
+++ client/components/charts/barchart.js
@@ -0,0 +1,74 @@
+import * as d3 from 'd3'
+
+export function drawChart (height, width, data) {
+  const svg = d3.select('#barchart')
+
+    .append('svg')
+
+    .attr('width', width)
+
+    .attr('height', height)
+
+    .style('border', '1px solid black')
+
+  var selection = svg.selectAll('rect').data(data)
+  var yScale = d3.scaleLinear()
+
+    .domain([0, d3.max(data)])
+
+    .range([0, height - 100])
+
+  selection
+
+    .transition().duration(300)
+
+    .attr('height', (d) => yScale(d))
+
+    .attr('y', (d) => height - yScale(d))
+
+  selection
+
+    .enter()
+
+    .append('rect')
+
+    .attr('x', (d, i) => i * 45)
+
+    .attr('y', (d) => height)
+
+    .attr('width', 40)
+
+    .attr('height', 0)
+
+    .attr('fill', 'orange')
+
+    .transition().duration(300)
+
+    .attr('height', (d) => yScale(d))
+
+    .attr('y', (d) => height - yScale(d))
+
+  selection
+
+    .exit()
+
+    .transition().duration(300)
+
+    .attr('y', (d) => height)
+
+    .attr('height', 0)
+
+    .remove()
+
+  // var i = 0
+
+  // function changeData () {
+  //   drawChart(datasets[i++])
+
+  //   if (i == datasets.length) i = 0
+  // }
+
+  // window.addEventListener('load', function () {
+  //   changeData()
+  // })
+}
