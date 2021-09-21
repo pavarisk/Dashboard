04130da9a494bcded812ebc56f7a8e069c93c24a
Andrew-Somewhat <andrewpuia054@gmail.com>
2021-09-21 12:23:00 +1200

    canvas prep

--- client/components/BarChart.jsx
+++ client/components/BarChart.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import { drawChart } from './charts/barchart'
 import { connect } from 'react-redux'
-
 function BarChart (props) {
   const { authenticated } = props
   const d3chart = useRef()
@@ -22,14 +21,16 @@ function BarChart (props) {
       .catch(e => console.log(e.message))
   }, [])
 
-  return (
-    <div className='d-flex flex-column'>
-      {authenticated.name && <h1 className='pt-4 text-center'>Hello {authenticated.name}</h1>}
-      <div id='barchart' className='d-flex flex-column align-items-center'>
+  return (<>
+    <canvas id="barchart" >
+      <div className='d-flex flex-column' >
+        {authenticated.name && <h1 className='pt-4 text-center'>Hello {authenticated.name}</h1>}
+        {/* <div id='barchart' className='d-flex flex-column align-items-center'> */}
         <svg ref={d3chart}></svg>
       </div>
-    </div>
-  )
+      {/* </div> */}
+    </canvas>
+  </>)
 }
 
 function mapStateToProps (state) {
---
+++ client/components/charts/canvas.js
@@ -0,0 +1,11 @@
+// import * as d3 from 'd3'
+
+// export function drawCanvas (width, height) {
+//   const svg = d3.select('#canvas')
+
+//   const canvas = document.getElementById('testCanvas')
+
+//   const ctx = canvas.getContext('2d')
+
+//   ctx.fillStyle('blue')
+// }
